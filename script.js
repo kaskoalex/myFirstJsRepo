6d207a5dae264d64f91fa31c175370599aaf773b
Alex <webmaster@papagai.de>
2022-01-27 19:04:34 +0100

    первый коммит

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-const a = "Переменная из lesson03"
-const b = "Наша ветка lesson01"
-
-const logged = () => {
-  console.log("Наконец мы узнали все команды, необходимые для работы с GIT")
-}
-
-console.log("Hello Word");
+const a = 1
+const b = "вторая переменная"
