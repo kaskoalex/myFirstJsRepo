8d357ef88bf4c7414f23e3fe83f87d335b61f3cb
Alex <webmaster@papagai.de>
2022-01-30 13:14:19 +0100

    X

---
+++ prob.js
@@ -0,0 +1,12 @@
+const num = 266219;
+// результат произведений цифр числа num;
+let numProduct = (num + '').split('').reduce((prod, numNum) => prod * +numNum, 1);
+
+console.log(`Произведение цифр числа ${num} равно ${numProduct}`);
+console.log('\n')
+console.log(`Первые две цифры ${numProduct}**3 равны ${(numProduct ** 3 + '').slice(0, 2)}`);
+
+
+console.log((num+'').split(''))
+
+console.log((num + '').split(''))
