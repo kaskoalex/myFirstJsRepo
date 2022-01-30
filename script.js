c8bbc99cb8c29c469c959bed2e0badf527044c6b
Alex <webmaster@papagai.de>
2022-01-30 13:13:12 +0100

    Prob

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const a = 1
-const b = "вторая переменная"
+let myVar;
+
+myVar=10;
+console.log(typeof myVar);
+myVar = 12345678902346784815567677646888588n;
+console.log(typeof myVar);
+myVar = "Hallo Word"
+console.log(typeof myVar);
+myVar=true;
+console.log(typeof myVar);
+myVar=null;
+console.log(typeof myVar);
+myVar=undefined;
+console.log(typeof myVar);
+
+console.log(.5)
+
+
+
