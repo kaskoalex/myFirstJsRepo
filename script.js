df35d19053a5f187e6b51bb57e023445f8c5eae1
Alex <webmaster@papagai.de>
2022-02-03 23:39:37 +0100

    fix

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
 'use strict';
 
-let y = 5
+//let y = 5
 
 
-function ones(c,) {
+//function ones(c,) {
   
-  console.log(y+c);
+//  console.log(y+c);
 
   
-}
+//}
 
-function two() { 
-  let y=15
-  ones(3)
- }
+//function two() { 
+//  let y=15
+//  ones(3)
+ //}
 
-two()
+//two()
 
 
 //-------------------------
 
+function one(х) {
+  function two() {
+
+    console.log(х);
+    console.dir(two);
+  }
+  two()
+
+
+}
+
+one(10)
+
+function counter(n) {
+  return function () {
+    return 1000+n
+  }
+}
+
+let sum=counter(1)
+console.log(sum())
+
+
+function pathGenerator(url) {
+  return function (imageName) { 
+    return url+imageName
+  }
+}
+
+let urlIcon =pathGenerator("http//mydomain.ru/icons/")
+let urlImag = pathGenerator("http//mydomain.ru/images/")
+
+console.log(urlIcon("cklock.svg"));
+console.log(urlImag("man.png"));
 
