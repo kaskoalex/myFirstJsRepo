cb689a54e3824ede510524876f469cd3e95ff983
Alex <webmaster@papagai.de>
2022-02-03 12:29:56 +0100

    create

--- script.js
+++ script.js
@@ -1,62 +1,46 @@
 'use strict';
 
-const potato=10
+let a = 5
 
-
-
-for (let i = 10; i > 0; i--) {
-  console.log("Картошка N" + i + " почищена");
+function one() {
+  let b = 10
+  console.log(b);
 }
 
+one()
 
-for (let i = 1; i < 11; i++) {
-  console.log("Картошка N" + i + " почищена");
+//console.log(b);
+//b  не выводится
+
+
+function one() {
+  b = 10
+  console.log(b);
 }
 
-for (let i = 1; i < 11; i++) {
-  if (i===5) continue  //пропустить итерацию n 5
-  console.log("Картошка N" + i + " почищена");
+one()
+
+//console.log(b);
+
+// b без let выводится если сверху не указано 'use strict';
+
+function one() {
+  let a = 8
+  console.log(a);
 }
 
-for (let i = 1; i < 11; i++) {
-  if (i === 5) break  //остановка
-  console.log("Картошка N" + i + " почищена");
+one()
+
+//-----------------------------------------------
+
+
+function ones() {
+  let a = 14
+  function two() {
+    let a = 11
+    console.log(a);
+  }
+  two()
 }
 
-for (let i = 1; i < 11; i++) {
-  if (i !== 5)   //пропустить итерацию n 5
-  console.log("Картошка N" + i + " почищена");
-}
-
-//const peelPotato = function(potatoIndex){
-  //if (potatoIndex>0){
-    //console.log("Картошка N" + potatoIndex +" по//чищена");
-    //potatoIndex--
-    //peelPotato(potatoIndex)
-  //}
-//}
-//-- рекурсия  --
-//peelPotato(potato)
-
-let potato = 10
-
-while (potato>0) {
-  console.log("Картошка N" + potato + " почищена");
-  potato--
-}
-
-//--------------------------------------------------
-
-
-
-
-
-
-
-//------------------------------------------------
-let potato = 10
-
-do {
-  console.log("Картошка N" + potato + " почищена");
-  potato--
-} while(false)
+ones()
