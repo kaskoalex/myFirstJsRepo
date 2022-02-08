1769e5c63a054c3a50a1c774aea3e2ab32412b78
Alex <webmaster@papagai.de>
2022-02-08 16:45:14 +0100

    create

--- script.js
+++ script.js
@@ -1,65 +1,40 @@
 'use strict';
 
-//console.log(document);
-//console.log()
-//console.log()
-//console.log()
-//console.log()
-//console.dir(document)
-//console.log()
-//console.log()
-//console.log()
-//console.log()
-//console.dir(document.childNodes)
-//console.log()
-//console.log()
-//console.dir(document.body)
+const title = document.getElementById("title")
+
+//title.classList.add("green-color")// dobawili k классу подклас зелёный цвет, который задан на этот клас в css
+//title.classList.remove("title")// удалили из класса
 
 
-// Работа s ID
-const title =document.getElementById("title")
+// title.classList.toggle("title")// togle удаляет если есть и добавляет если нет
+
+
+console.log(title.classList.contains("title"));// false or truе в зависимости от того есть еллемент или нет
+
+
 console.log(title);
 
-console.dir(title); // в виде объекта
 
-console.dir(title.style) // описание стилей
+console.log(title.getAttribute("title"))// объект
+title.setAttribute("title", "new value title")// поменяли значение атрибута
 
-console.dir(title.textContent = "DOM!")  // изменение текста
-console.dir(title.style.backgroundColor = "green") // изменение цвета
-//---------------------------------------------------
-const listItems = document.getElementsByTagName("li")
-console.log(listItems); 
-// псевдомассив
-console.log(listItems[0]); // по индексу или через цыкл
-//-----------------------------------------------------
+console.log(title.getAttribute("title"))
 
-const links = document.getElementsByClassName("link")
-console.log(links); // массив еллементов через класс
+console.dir(title);
+//-----------------------------------------------------------
 
-//-----------------------------------------------------
+title.setAttribute("id", "title2")// поменяли значение атрибута
+console.log(title);
 
-//const title1 = document.querySelector("#title") // вызов как в css / через ID
-//const title1 = document.querySelector("h1") //через тег
+//---------------------------------------------------------
 
-const title1 = document.querySelector(".title") // через класс
-console.log(title1)
-
-const title2 = document.querySelector('[title="Document Object Model"]') // через атрибут
-console.log(title2)
-
-const header = document.querySelector("header > ul ") 
-console.log(header)
-//--------------------------------------------------------
-const link2=document.querySelectorAll(".link")
-console.log(link2[1]) // получаем нод лист , где можно вызывать через индекс
-
-console.log(link2);
-link2.forEach(function (param) { 
-  console.log(param)
-})  //  querySelector
+console.log(title.hasAttribute("title2"))// prowerka na nalicie атрибута
 
 
-const list = document.querySelector("header > ul ")
-const listItem2 = list.querySelectorAll("li")
+//---------------------------------------------------------
 
-console.log(listItem2)
+title.style.backgroundColor='green'// добавили зел цвет бекграунд
+console.dir(getComputedStyle(title)); // посмотреть свойства css
+console.dir(getComputedStyle(title).backgroundColor);// посмотреть цвет
+console.dir(getComputedStyle(title).fontSize);
+console.log(title);
