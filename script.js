94e9da009550e2707e5a447841a047a9b500b616
Alex <webmaster@papagai.de>
2022-02-08 15:08:04 +0100

    create

--- script.js
+++ script.js
@@ -1,57 +1,65 @@
 'use strict';
 
-//let y = 5
+//console.log(document);
+//console.log()
+//console.log()
+//console.log()
+//console.log()
+//console.dir(document)
+//console.log()
+//console.log()
+//console.log()
+//console.log()
+//console.dir(document.childNodes)
+//console.log()
+//console.log()
+//console.dir(document.body)
 
 
-//function ones(c,) {
-  
-//  console.log(y+c);
+// Работа s ID
+const title =document.getElementById("title")
+console.log(title);
 
-  
-//}
+console.dir(title); // в виде объекта
 
-//function two() { 
-//  let y=15
-//  ones(3)
- //}
+console.dir(title.style) // описание стилей
 
-//two()
+console.dir(title.textContent = "DOM!")  // изменение текста
+console.dir(title.style.backgroundColor = "green") // изменение цвета
+//---------------------------------------------------
+const listItems = document.getElementsByTagName("li")
+console.log(listItems); 
+// псевдомассив
+console.log(listItems[0]); // по индексу или через цыкл
+//-----------------------------------------------------
+
+const links = document.getElementsByClassName("link")
+console.log(links); // массив еллементов через класс
+
+//-----------------------------------------------------
+
+//const title1 = document.querySelector("#title") // вызов как в css / через ID
+//const title1 = document.querySelector("h1") //через тег
+
+const title1 = document.querySelector(".title") // через класс
+console.log(title1)
+
+const title2 = document.querySelector('[title="Document Object Model"]') // через атрибут
+console.log(title2)
+
+const header = document.querySelector("header > ul ") 
+console.log(header)
+//--------------------------------------------------------
+const link2=document.querySelectorAll(".link")
+console.log(link2[1]) // получаем нод лист , где можно вызывать через индекс
+
+console.log(link2);
+link2.forEach(function (param) { 
+  console.log(param)
+})  //  querySelector
 
 
-//-------------------------
+const list = document.querySelector("header > ul ")
+const listItem2 = list.querySelectorAll("li")
 
-function one(х) {
-  function two() {
-
-    console.log(х);
-    console.dir(two);
-  }
-  two()
-
-
-}
-
-one(10)
-
-function counter(n) {
-  return function () {
-    return 1000+n
-  }
-}
-
-let sum=counter(1)
-console.log(sum())
-
-
-function pathGenerator(url) {
-  return function (imageName) { 
-    return url+imageName
-  }
-}
-
-let urlIcon =pathGenerator("http//mydomain.ru/icons/")
-let urlImag = pathGenerator("http//mydomain.ru/images/")
-
-console.log(urlIcon("cklock.svg"));
-console.log(urlImag("man.png"));
-
+console.log(listItem2)
