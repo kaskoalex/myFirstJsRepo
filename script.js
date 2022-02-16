9d5e9d340c89488889e927e995c2e1fc9cbca8e0
Alex <webmaster@papagai.de>
2022-02-16 17:30:42 +0100

    create

--- script.js
+++ script.js
@@ -1,65 +1,93 @@
-'use strict';
-
-//console.log(document);
-//console.log()
-//console.log()
-//console.log()
-//console.log()
-//console.dir(document)
-//console.log()
-//console.log()
-//console.log()
-//console.log()
-//console.dir(document.childNodes)
-//console.log()
-//console.log()
-//console.dir(document.body)
 
 
-// Работа s ID
-const title =document.getElementById("title")
-console.log(title);
+/*
+const user1 = { 
+  name: 'Alex',
+  say: function () { 
+    console.log(this.name); // вызывает имя этого (this) объекта
+   }
+}
 
-console.dir(title); // в виде объекта
+const user2 = {
+  name: 'Vlad',
+  say: function () {
+    console.log(this.name);
+  }
+}
 
-console.dir(title.style) // описание стилей
+user1.say()
+user2.say()
 
-console.dir(title.textContent = "DOM!")  // изменение текста
-console.dir(title.style.backgroundColor = "green") // изменение цвета
-//---------------------------------------------------
-const listItems = document.getElementsByTagName("li")
-console.log(listItems); 
-// псевдомассив
-console.log(listItems[0]); // по индексу или через цыкл
-//-----------------------------------------------------
+*/
 
-const links = document.getElementsByClassName("link")
-console.log(links); // массив еллементов через класс
+/*
 
-//-----------------------------------------------------
+const user1 = {
+  name: 'Alex',
+  say: say
+  
+}
 
-//const title1 = document.querySelector("#title") // вызов как в css / через ID
-//const title1 = document.querySelector("h1") //через тег
+const user2 = {
+  name: 'Vlad',
+  say: say
+  
+}
 
-const title1 = document.querySelector(".title") // через класс
-console.log(title1)
+function say(a,b) {
+  console.log(a+b)
+  console.log(this.name); // this - указывает на user ; say() без user не работает
+}
 
-const title2 = document.querySelector('[title="Document Object Model"]') // через атрибут
-console.log(title2)
+user1.say(1,2)
+user2.say(1,2)
 
-const header = document.querySelector("header > ul ") 
-console.log(header)
-//--------------------------------------------------------
-const link2=document.querySelectorAll(".link")
-console.log(link2[1]) // получаем нод лист , где можно вызывать через индекс
+say.call(user1, 1, 2)
+say.apply(user2,[1,2])  // applay принимает второй аргумент в виде массива
 
-console.log(link2);
-link2.forEach(function (param) { 
-  console.log(param)
-})  //  querySelector
+*/
+
+const user1 = {
+  name: 'Alex',
+  say: say
+
+}
+
+const user2 = {
+  name: 'Vlad',
+  say: say
+
+}
+
+function say() {
+
+  console.log(this.name);
+}
+
+const newSay = say.bind(user1)
+newSay()
+
+//------------------------------------------------
+
+const btns = document.querySelectorAll('button')
+
+const chaColorBorder = function () {
+  this.style.border = '2px solid red'
+}
+
+btns.forEach(function (btn) {
+  btn.addEventListener('click, chaColorBorder)')
+})
 
 
-const list = document.querySelector("header > ul ")
-const listItem2 = list.querySelectorAll("li")
+//------------------------------------------------------
 
-console.log(listItem2)
+const counter = (a, b) => a + b; //или с возвратом return
+
+const rep = (a, b) => {
+  return a + b
+}
+
+const rep = a => a + 3
+
+console.log(counter(1, 4))
